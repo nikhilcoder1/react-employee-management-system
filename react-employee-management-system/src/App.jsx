cdbf28ba2a98222d2b057728116c404769f7ee2d
dev_nick <developernikhil28@gmail.com>
2026-01-27 12:46:17 +0530

    Login Route added

--- react-employee-management-system/src/App.jsx
+++ react-employee-management-system/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/Auth/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className='text-amber-600 text-7xl'>App</h1>
+    <>
+      <Login />
+    </>
   )
 }
 
